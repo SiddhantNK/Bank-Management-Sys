d1412c24e6a64cd47ea1734cb1b5a2495471656b
Janvhi Kardure <janvhi.1251050199@vit.edu>
2025-12-20 20:52:49 +0530

    terminal colors testing

--- test.c
+++ test.c
@@ -3,10 +3,15 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <time.h>
-#define FILENAME "user1.txt"
-#define MAX_SIZE 50
-
-
+#define RESET "\033[0m" 
+  #define RED "\033[31m"   // warnings, exit option
+  #define CYAN "\033[36m"  
+    #define GREEN "\033[32m" // success/selection
+    #define YELLOW "\033[33m" // in progress
+      #define WHITE "\033[37m" //normal text
+      #define BLUE "\033[34m"  // input prompts
+        #define BOLD "\033[1m" // title
+      
 struct users
 {
   char username[100];
@@ -15,13 +20,20 @@ struct users
  char time_buffer[80];
 
 
+int in_prog()
+{
+  printf("\e[1;1H\e[2J");
+  printf("In progress...\n Press any key to return");
+  getch();
+}
+
 int account_info(char username[])
 {
   int act = 0, enter = 0;
   char ch;
   printf("\e[1;1H\e[2J");
   printf("Welcome, %s!\n", username);
-  printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
+  printf(WHITE"What do you wanna do today?\nPress number keys to select between options and then press Enter\n"RESET);
   printf("  1.See account information\n  2.Transfer money\n  3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
 
   while (enter != 1)
@@ -33,8 +45,12 @@ int account_info(char username[])
       act = 1;
       printf("\e[1;1H\e[2J");
       printf("Welcome, %s\n!", username);
-      printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
-      printf("=>1.See account information\n  2.Transfer money\n  3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
+      printf(WHITE"What do you wanna do today?\nPress number keys to select between options and then press Enter\n"RESET);
+      printf(GREEN"=>1.See account information\n"RESET);
+      printf (WHITE"2.Transfer money\n"RESET);
+      printf (BLUE"3.Change password\n"RESET);
+      printf(BLUE"4.Change email\n"RESET);
+      printf (RED"5.Back to Login/Sign Up"RESET);
       break;
     case '2':
       act = 2;
@@ -67,156 +83,82 @@ int account_info(char username[])
     case '\r':
       if (act == 1)
       {
-        printf("\e[1;1H\e[2J");
-        printf("In progress...\n Press any key to return");
-        getch();
-        act = 1;
-        printf("\e[1;1H\e[2J");
-        printf("Welcome, %s\n", username);
-        printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
-        printf("=>1.See account information\n  2.Transfer money\n  3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
+        in_prog();
       }
       else if (act == 2)
       {
-        printf("\e[1;1H\e[2J");
-        printf("In progress...\n Press any key to return");
-        getch();
-        printf("\e[1;1H\e[2J");
-        printf("Welcome, %s\n", username);
-        printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
-        printf("  1.See account information\n=>2.Transfer money\n  3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
+        in_prog();
       }
       else if (act == 3)
       {
-        printf("\e[1;1H\e[2J");
-        printf("In progress...\n Press any key to return");
-        getch();
-        printf("\e[1;1H\e[2J");
-        printf("Welcome, %s\n", username);
-        printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
-        printf("  1.See account information\n  2.Transfer money\n=>3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
+        in_prog();
       }
       else if (act == 4)
       {
-        printf("\e[1;1H\e[2J");
-        printf("In progress...\n Press any key to return");
-        getch();
-        printf("\e[1;1H\e[2J");
-        printf("Welcome, %s\n", username);
-        printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
-        printf("  1.See account information\n  2.Transfer money\n  3.Change password\n=>4.Change email\n  5.Back to Login/Sign Up");
+        in_prog();
       }
       else
       {
         enter = 1;
       }
-      break;
+
     default:
+      printf("\e[1;1H\e[2J");
+      printf("Welcome, %s!\n", username);
+      printf("What do you wanna do today?\nPress number keys to select between options and then press Enter\n");
+      printf("  1.See account information\n  2.Transfer money\n  3.Change password\n  4.Change email\n  5.Back to Login/Sign Up");
       break;
     }
   }
-  // getch();
-}
-int authenticate_user(const char *input_username, const char *input_password) {
-    FILE *file = fopen(FILENAME, "r");
-
-    char file_username[MAX_SIZE];
-    char file_password[MAX_SIZE];
-    int authenticated = 0;
-
-    while (fscanf(file, "%s %s", file_username, file_password) == 2) {
-        
-        if (strcmp(input_username, file_username) == 0 &&
-            strcmp(input_password, file_password) == 0) {
-            
-            authenticated = 1; 
-            break;
-        }
-    }
-
-    fclose(file); 
-    return authenticated;
+  return 0;
 }
 
 int login()
 {
+  printf("\e[1;1H\e[2J");
+  struct users userLogin;
 
-    printf("\e[1;1H\e[2J"); 
-    
-    struct users userLogin;
-    int attempt_count = 0; 
-    int authenticated = 0;
+  printf(BLUE"Enter your username: "RESET);
+  scanf("%s", userLogin.username);
 
-
-    while (1) { 
-        attempt_count++;
-        
-        printf("--- User Login System ---\n");
-        printf("Please log in to continue.\n", attempt_count);
-        
-      
-        printf("Enter your username: ");
-        if (scanf("%s", userLogin.username) != 1) {
-        
-            printf("\nInput error. Starting next attempt.\n");
-            continue; 
-        }
-
-       
-        int i = 0;
-        char ch;
-        printf("Enter your password: ");
-        
-        while ((ch = getch()) != '\r' && i < MAX_SIZE - 1) // \r is Enter key
-        {
-            if (ch == 8) 
-            {
-                if (i > 0)
-                {
-                    printf("\b \b"); 
-                    userLogin.password[--i] = '\0';
-                }
-            }
-            else
-            {
-                printf("*"); 
-                userLogin.password[i++] = ch;
-            }
-        }
-        userLogin.password[i] = '\0'; 
-        printf("\n"); 
-
-       
-        authenticated = authenticate_user(userLogin.username, userLogin.password);
-
-        if (authenticated) {
-            printf("\n LOGIN SUCCESSFUL! Welcome back %s.\n", userLogin.username);
-           
-            account_info(userLogin.username); 
-            break;
-        } else {
-            printf("\e[1;1H\e[2J"); 
-            printf("\n Invalid username or password. Please try again.\n");
-            
-            
-            
-        }
+  int i = 0;
+  char ch;
+  printf(BLUE"Enter your password: "RESET);
+  while ((ch = getch()) != '\r')
+  {
+    if (ch == 8)
+    {
+      if (i > 0)
+      {
+        printf("\b \b");
+        userLogin.password[i--] = '\0';
+      }
     }
-    
-    return 0; 
+    else
+    {
+      printf("*"); // enter is '\r'
+      userLogin.password[i++] = ch;
+    }
+  }
+  userLogin.password[i] = '\0'; // string terminate with \0 so we remove it by ts
+
+  printf(YELLOW"\nYour user name is: %s"RESET, userLogin.username);
+  printf(YELLOW"\nYour password is: %s"RESET, userLogin.password);
+  account_info(userLogin.username);
+  return 0;
 }
 
 int signup()
 {
   struct users userSignIn;
   printf("\e[1;1H\e[2J");
-  printf("Enter your username: ");
+  printf(BLUE"Enter your username: "RESET);
   scanf("%s", userSignIn.username);
   // getch();
 
   int i = 0;
   char en;
-  printf("Enter your password: ");
+  printf(BLUE"Enter your password: "RESET);
   while ((en = getch()) != '\r')
   {
     if (en == 8)
@@ -234,23 +176,17 @@ int signup()
     }
   }
   userSignIn.password[i] = '\0';
-    time_t rawtime;
-    struct tm *timeinfo;
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    strftime(time_buffer, sizeof(time_buffer), "[%d/%m/%Y %I:%M:%S %p]", timeinfo);
-    // strftime(time_buffer, sizeof(time_buffer), "[%Y-%m-%d %H:%M:%S]", timeinfo);
+ 
 
-  FILE *new = fopen("user1.txt", "a"); // open  modes: "rb", "ab", "wb"
-  // fprintf(new, "%s %s\n", userSignIn.username, userSignIn.password);
-  // fprintf(new, "%s %s %s\n", time_buffer, userSignIn.username, userSignIn.password);`
-  fprintf(new, "%s %s %s\n",  userSignIn.username, userSignIn.password , time_buffer);
+  FILE *new = fopen("users1.txt", "a"); // open  modes: "rb", "ab", "wb"
+  fprintf(new, "%s %s\n", userSignIn.username, userSignIn.password);
+  
 
-  printf("\nYour user name is: %s \n", userSignIn.username); 
-  printf("Your password is: %s .\nRemember your credentials for future Login.\n", userSignIn.password);
+  printf(YELLOW"\nYour user name is: %s \n"RESET, userSignIn.username); 
+  printf(YELLOW"Your password is: %s \n"RESET, userSignIn.password);
 
   fclose(new);
-  printf("Your account has been created succesfully.");
+  printf(GREEN"\nYour account has been created succesfully."RESET);
   getch();
   account_info(userSignIn.username);
   return 0;
@@ -258,7 +194,7 @@ int signup()
 
 int user_menu()
 {
-  int enter, menu = 0;
+  int enter = 0, menu = 0;
   char ch;
 
   printf("\e[1;1H\e[2J");
@@ -274,46 +210,55 @@ int user_menu()
       menu = 1;
       printf("\e[1;1H\e[2J");
       printf("Press number keys to select between options and then press Enter\n");
-      printf("\n>1.Login\n 2.Create an account\n 3.Exit to main screen");
+      printf(GREEN">1.Login\n"RESET);
+        printf(WHITE"2.Create an account\n"RESET);
+        printf (RED"3.Exit to main screen"RESET);
       break;
     case '2':
       menu = 2;
       printf("\e[1;1H\e[2J");
       printf("Press number keys to select between options and then press Enter\n");
-      printf("\n 1.Login\n>2.Create an account\n 3.Exit to main screen");
+      printf(WHITE" 1.Login\n"RESET);
+      printf(GREEN">2.Create an account\n"RESET);
+      printf (RED"3.Exit to main screen"RESET);
       break;
     case '3':
       menu = 3;
       printf("\e[1;1H\e[2J");
       printf("Press number keys to select between options and then press Enter\n");
-      printf("\n 1.Login\n 2.Create an account\n>3.Exit to main screen");
+      printf(WHITE"\n 1.Login\n"RESET);
+      printf (WHITE"2.Create an account\n"RESET);
+      printf(RED">3.Exit to main screen"RESET);
       break;
     case '\r':
       if (menu == 1)
       {
         login();
-        menu = 1;
-        printf("\e[1;1H\e[2J");
-        printf("Press number keys to select between options and then press Enter\n");
-        printf("\n>1.Login\n 2.Create an account\n 3.Exit to main screen");
       }
       else if (menu == 2)
       {
         signup();
-        menu = 2;
-        printf("\e[1;1H\e[2J");
-        printf("Press number keys to select between options and then press Enter\n");
-        printf("\n 1.Login\n>2.Create an account\n 3.Exit to main screen");
       }
       else
       {
         enter = 1;
       }
-      break;
     default:
+      printf("\e[1;1H\e[2J");
+      printf(WHITE"Press number keys to select between options and then press Enter\n"RESET);
+      printf("\n 1.Login\n 2.Create an account\n 3.Exit to main screen");
       break;
     }
   }
+  return 0;
+}
+
+int employee_menu()
+{
+  printf("\e[1;1H\e[2J");
+  printf(YELLOW"In Progress...Please come back later\nPress any key to return"RESET);
+  getch();
+  return 0;
 }
 
 int main()
@@ -322,8 +267,8 @@ int main()
   char ch;
 
   printf("\e[1;1H\e[2J");
-  printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-  printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
+  printf(BOLD CYAN "-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n"RESET);
+  printf(WHITE"What do you wanna do? \n Press number keys to select between options and then press Enter\n"RESET);
   printf("  1.Employee Platform \n  2.Customer Platform\n  3.Exit Application");
 
   while (enter != 1)
@@ -334,44 +279,40 @@ int main()
     case '1':
       choice = 1;
       printf("\e[1;1H\e[2J");
-      printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-      printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
-      printf("=>1.Employee Platform \n  2.Customer Platform\n  3.Exit Application");
+      printf(BOLD CYAN"-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n"RESET);
+      printf(WHITE"What do you wanna do? \nPress number keys to select between options and then press Enter\n"RESET);
+      printf(GREEN"=>1.Employee Platform \n" RESET);
+        printf(WHITE"2.Customer Platform\n" RESET);
+          printf(RED"3.Exit Application"RESET);
       break;
     case '2':
       choice = 2;
       printf("\e[1;1H\e[2J");
-      printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-      printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
-      printf("  1.Employee Platform \n=>2.Customer Platform\n  3.Exit Application");
+      printf(BOLD CYAN"-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n"RESET);
+      printf(WHITE"What do you wanna do? \nPress number keys to select between options and then press Enter\n"RESET);
+      printf(WHITE"  1.Employee Platform \n"RESET);
+        printf(GREEN"=>2.Customer Platform\n"RESET);
+          printf(RED"3.Exit Application"RESET);
       break;
     case '3':
       choice = 3;
       printf("\e[1;1H\e[2J");
-      printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-      printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
-      printf("  1.Employee Platform \n  2.Customer Platform\n=>3.Exit Application");
+      printf(BOLD CYAN"-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n"RESET);
+      printf(WHITE"What do you wanna do? \nPress number keys to select between options and then press Enter\n"RESET);
+      printf(WHITE"  1.Employee Platform \n"RESET);
+       printf(WHITE"2.Customer Platform\n" RESET);
+       printf(RED"=>3.Exit Application"RESET);
       break;
     case '\r':
       if (choice == 1)
       {
+        employee_menu();
         choice = 1;
-        printf("\e[1;1H\e[2J");
-        printf("In Progress...Please come back later\nPress any key to return");
-        getch();
-        printf("\e[1;1H\e[2J");
-        printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-        printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
-        printf("=>1.Employee Platform \n  2.Customer Platform\n  3.Exit Application");
       }
       else if (choice == 2)
       {
         user_menu();
         choice = 2;
-        printf("\e[1;1H\e[2J");
-        printf("-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n");
-        printf("What do you wanna do? \nPress number keys to select between options and then press Enter\n");
-        printf("  1.Employee Platform \n=>2.Customer Platform\n  3.Exit Application");
       }
       else
       {
@@ -379,6 +320,10 @@ int main()
       }
 
     default:
+      printf("\e[1;1H\e[2J");
+      printf(BOLD CYAN"-*-*-*-*- LAXMI CHIT FUND -*-*-*-*-\n"RESET);
+      printf(WHITE"What do you wanna do? \nPress number keys to select between options and then press Enter\n"RESET);
+      printf("  1.Employee Platform \n  2.Customer Platform\n  3.Exit Application");
       break;
     }
   }
